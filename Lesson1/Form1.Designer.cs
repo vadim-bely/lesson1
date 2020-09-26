3f4f045530181165d21a890c895229be5ac7c11e
Vadim Bely <vadim.belyy.02@mail.ru>
2020-09-26 09:41:19 +0300

    Initial commit

---
+++ Lesson1/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Lesson1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(242, 22);
+            this.button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(227, 128);
+            this.button.TabIndex = 0;
+            this.button.Text = "нажми на меня";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.BackColor = System.Drawing.SystemColors.Control;
+            this.label.Font = new System.Drawing.Font("Times New Roman", 14F);
+            this.label.ForeColor = System.Drawing.Color.Red;
+            this.label.Location = new System.Drawing.Point(360, 210);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(86, 27);
+            this.label.TabIndex = 1;
+            this.label.Text = "Привет";
+            this.label.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(682, 296);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Тыкалка";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label label;
+    }
+}
+
